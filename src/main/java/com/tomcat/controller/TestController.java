397613b6102f143b12a05778f3e1d787bb794c8e
akshay <waghmarea2@gmail.com>
2026-02-19 22:07:20 +0530

    jenkinsfile modify

--- src/main/java/com/tomcat/controller/TestController.java
+++ src/main/java/com/tomcat/controller/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
 
     @GetMapping("/")
     public String home() {
-        return "CICD Tomcat deployment working !!! test test test";
+        return "CICD Tomcat deployment working !!!";
     }
 }
 
