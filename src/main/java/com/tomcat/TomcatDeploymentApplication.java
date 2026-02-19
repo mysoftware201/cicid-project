a160fa399a25abf73f2faafa1db9af7f36aad01d
akshay <waghmarea2@gmail.com>
2026-02-19 13:21:11 +0530

    tomcat deployment 1

---
+++ src/main/java/com/tomcat/TomcatDeploymentApplication.java
@@ -0,0 +1,21 @@
+package com.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TomcatDeploymentApplication
+        extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TomcatDeploymentApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatDeploymentApplication.class, args);
+    }
+}
+
---
+++ src/main/java/com/tomcat/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.tomcat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Tomcat deployment working 🚀";
+    }
+}
+
