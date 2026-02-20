ad0b9bd86fcf7f2b63451fe81ae5f3f767f1d48b
akshay <waghmarea2@gmail.com>
2026-02-20 13:48:07 +0530

    logs added

--- src/main/java/com/tomcat/controller/TestController.java
+++ src/main/java/com/tomcat/controller/TestController.java
@@ -1,14 +1,27 @@
 package com.tomcat.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class TestController 
+{
+	private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/")
     public String home() {
+    	log.info("cicd deployed on tomcat server");
         return "CICD Tomcat deployment working !!!";
     }
+    
+    @GetMapping("/welcome")
+    public String welcome() {
+    	log.info("Welcome Endpoint -> cicd deployed on tomcat server");
+        return "Welcome Endpoint --> CICD Tomcat deployment working !!!";
+    }
+    
+    
 }
 
